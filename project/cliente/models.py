0e5a6bdc89c007e5e7cedadf8b09fa72f9f4602b
juan faura <juanfaura51000@gmail.com>
2023-06-27 21:14:52 -0300

    quecapo

--- project/cliente/models.py
+++ project/cliente/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Pais(models.Model):
     nombre = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return self.nombre
+
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    nacimiento = models.DateField(Null=True)
+    nacimiento = models.DateField(null=True)
     pais_origen_id = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
